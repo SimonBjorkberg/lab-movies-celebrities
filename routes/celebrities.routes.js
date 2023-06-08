9fb96c5b7b180a7f886c8eee6b2bb88bcf772fa0
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-08 09:56:57 +0200

    finished iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,21 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/celebrities/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post('/celebrities/create', (req, res) => {
+    const { name, occupation, catchPhrase} = req.body
+        Celebrity.create({name, occupation, catchPhrase})
+        .then(celebFromDB => console.log(`new celeb ${celebFromDB}`))
+        .catch(err => {
+            console.log('err', err)
+        res.redirect('/celebrities/create')
+        })
+        res.redirect('/celebrities')
+})
 
 module.exports = router;
